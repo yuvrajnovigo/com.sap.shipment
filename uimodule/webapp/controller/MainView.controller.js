3c73a25e73ef2793ec7dd47480d583d2b565dd2d
yuvraj <yuvraj.puri@novigo.com>
2020-10-06 16:31:43 +0530

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/sap/shipment/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.sap.shipment.controller.MainView", {});
+});
